068f33d43d1816ade15246e7777f2a748ca2c4a4
Fred <fred@nanit.be>
2019-11-11 18:16:58 +0100

    F index file not needed

--- src/survey/store.js
+++ src/survey/store.js
@@ -1,10 +1,10 @@
-import {applyMiddleware, combineReducers, createStore} from 'redux/index'
-import thunk from 'redux-thunk/index';
+import {applyMiddleware, combineReducers, createStore} from 'redux'
+import thunk from 'redux-thunk';
 import {dataFetching} from "./base/reducer";
 
 const reducers = combineReducers({
     data : dataFetching,
-})
+});
 
 const store = createStore(reducers, applyMiddleware(thunk))
 
